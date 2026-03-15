e95f6d9570acf2f9176a25a2617093e662df6d81
deethorn <dchizota@gmail.com>
2026-03-15 00:15:15 +0000

    Changed Env and server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,23 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean);
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
